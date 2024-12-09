e333edbc78e11aff1a5d29c1cc40f69da1c3475e
SWARAJ DAS <151845349+swaraj-das@users.noreply.github.com>
2024-12-09 14:28:44 +0530

    completed Circular_LinkedList.c

--- Linked List/Circular_LinkedList.c
+++ Linked List/Circular_LinkedList.c
@@ -42,6 +42,108 @@ void cllInsertAtEnd(int item){
     last=temp;
 }
 
+void cllInsertAtAny(int pos, int item) {
+    struct Node *temp, *ptr;
+    temp = (struct Node*)malloc(sizeof(struct Node));
+    if (temp == NULL) {
+        printf("Insufficient Memory !!\n");
+        return;
+    }
+    temp->data = item;
+    if (pos == 1) {
+        if (last == NULL) {
+            last = temp;
+            temp->link = temp; // Fix: Properly initialize circular link
+        } else {
+            temp->link = last->link;
+            last->link = temp;
+        }
+    } else {
+        if (last == NULL) {
+            printf("Invalid Position !!\n");
+            free(temp);
+            return;
+        }
+        ptr = last->link;
+        for (int i = 1; i < pos - 1; i++) { // Fix: Start loop from 1 and stop at pos - 1
+            ptr = ptr->link;
+            if (ptr == last->link) { // Loop back to starting point
+                printf("Invalid Position !!\n");
+                free(temp);
+                return;
+            }
+        }
+        temp->link = ptr->link;
+        ptr->link = temp;
+        if (ptr == last) {
+            last = temp;
+        }
+    }
+}
+
+void cllDeleteFromBegin(){
+    struct Node *ptr;
+    if(last==NULL){
+        printf("Circular Linked List is Empty !!\n");
+        return;
+    }
+    ptr=last->link;
+    if(last==last->link){
+        last=NULL;
+    }
+    else{
+        last->link=ptr->link;
+    }
+    free(ptr);
+    ptr=NULL;
+}
+
+void cllDeleteFromEnd(){
+    struct Node *ptr, *prevptr;
+    if(last==NULL){
+        printf("Circular Linked List is Empty !!\n");
+        return;
+    }
+    ptr=last->link;
+    if(last==last->link){
+        last=NULL;
+    }
+    else{
+        while(ptr!=last){
+            prevptr=ptr;
+            ptr=ptr->link;
+        }
+        prevptr->link=last->link;
+        last=prevptr;
+    }
+    free(ptr);
+    ptr=NULL;
+}
+
+void cllDeleteFromAny(int pos) {
+    struct Node *ptr, *prevptr;
+    ptr=last->link;
+    if(pos==1){
+        cllDeleteFromBegin();
+    }
+    else{
+        for(int i=1; i<=pos-1; i++){
+            prevptr=ptr;
+            ptr=ptr->link;
+            if(ptr==last->link){
+                printf("Invalid Position!!\n");
+                return;
+            }
+        }
+        prevptr->link=last->link;
+        if(ptr=last){
+            last=prevptr;
+        }
+    }
+    free(ptr);
+    ptr=NULL;
+}
+
 void cllDisplay(){
     struct Node *ptr;
     if(last==NULL){
@@ -58,7 +160,7 @@ void cllDisplay(){
 
 int main(void){
     printf("\n---------Circular LinkList-----------\n");
-    int ch, data;
+    int ch, data, pos;
     while(1){
         printf("\n1. Insert at Begin");
         printf("\n2. Insert at End");
@@ -66,7 +168,7 @@ int main(void){
         printf("\n4. Delete from Begin");
         printf("\n5. Delete from End");
         printf("\n6. Delete from any Position");
-        printf("\n7. Display");
+        printf("\n7. Display");     
         printf("\n8. Exit");
         printf("\nEnter your choice: ");
         scanf("%d",&ch);
@@ -82,12 +184,22 @@ int main(void){
                 cllInsertAtEnd(data);
                 break;
             case 3:
+                printf("Enter the position: ");
+                scanf("%d", &pos);
+                printf("The data to the inserted: ");
+                scanf("%d", &data);
+                cllInsertAtAny(pos, data);
                 break;
             case 4:
+                cllDeleteFromBegin();
                 break;
             case 5:
+                cllDeleteFromEnd();
                 break;
             case 6:
+                printf("The position: ");
+                scanf("%d", &pos);
+                cllDeleteFromAny(pos);
                 break;
             case 7:
                 cllDisplay();
