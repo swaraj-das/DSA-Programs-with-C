c4985f848566c2c5e9816e25452f270c7955c1f0
SWARAJ DAS <151845349+swaraj-das@users.noreply.github.com>
2024-12-09 20:24:43 +0530

    Completed Single_LinkedList.c

--- Linked List/Single_LinkedList.c
+++ Linked List/Single_LinkedList.c
@@ -1,154 +1,228 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct node{
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
 	int data;
-	struct node* link;
+	struct node *link;
 };
-struct node* start=NULL;
-                      
-void sllInsertAtBegin(int item){	
-	struct node* temp; 
-	
-	temp =(struct node*) malloc(sizeof(struct node));
-	if(temp==NULL){
+struct node *start = NULL;
+
+void sllInsertAtBegin(int item)
+{
+	struct node *temp;
+
+	temp = (struct node *)malloc(sizeof(struct node));
+	if (temp == NULL)
+	{
 		printf("Insufficient Memory !!!");
 		return;
 	}
 
 	temp->data = item;
-	temp->link = start; 
-	start=temp;	
+	temp->link = start;
+	start = temp;
 }
 
-void sllInsertAtEnd(int item){
-	struct node* temp, *ptr;
-	temp=(struct node*) malloc(sizeof(struct node));
-	if(temp==NULL){
-        printf("Insufficient Memory !!!");
-        return;
-    }
+void sllInsertAtEnd(int item)
+{
+	struct node *temp, *ptr;
+	temp = (struct node *)malloc(sizeof(struct node));
+	if (temp == NULL)
+	{
+		printf("Insufficient Memory !!!");
+		return;
+	}
 	temp->data = item;
 	temp->link = NULL;
 
-	if(start==NULL){
-        start=temp;
-    }
-	else{
-		ptr=start;
-        while(ptr->link!=NULL)
-            ptr=ptr->link;
-        ptr->link=temp;
+	if (start == NULL)
+	{
+		start = temp;
+	}
+	else
+	{
+		ptr = start;
+		while (ptr->link != NULL)
+			ptr = ptr->link;
+		ptr->link = temp;
 	}
 }
 
-void sllInsertAtanyPos(int pos, int item){
-	struct node* temp, *ptr;
-	temp=(struct node*) malloc(sizeof(struct node));
-	if(temp==NULL){
-        printf("Insufficient Memory !!!");
-        return;
-    }
-	temp->data = item;
-	if(pos==1){
-		temp->link = start; 
-		start=temp;	
+void sllInsertAtanyPos(int pos, int item)
+{
+	struct node *temp, *ptr;
+	temp = (struct node *)malloc(sizeof(struct node));
+	if (temp == NULL)
+	{
+		printf("Insufficient Memory !!!");
+		return;
 	}
-	else{
-		ptr=start;
-		for(int i=1; i<=pos-2 && ptr!=NULL; i++){
-			ptr=ptr->link;
+	temp->data = item;
+	if (pos == 1)
+	{
+		temp->link = start;
+		start = temp;
+	}
+	else
+	{
+		ptr = start;
+		for (int i = 1; i <= pos - 2 && ptr != NULL; i++)
+		{
+			ptr = ptr->link;
 		}
-		if(ptr==NULL){
+		if (ptr == NULL)
+		{
 			printf("Invalid Position !!!!");
 
 			free(temp);
-			temp=NULL;	
-			return;		
+			temp = NULL;
+			return;
 		}
-		temp->link=ptr->link; 
-		ptr->link=temp;
+		temp->link = ptr->link;
+		ptr->link = temp;
 	}
 }
 
-void sllDeleteAtBegin(){
-	struct node* temp;
-    if(start==NULL){
-        printf("List is empty.\n");
-        return;
-    }
-    temp=start;
-    start=start->link;
-    free(temp);
-	temp=NULL;
+void sllDeleteAtBegin()
+{
+	struct node *temp;
+	if (start == NULL)
+	{
+		printf("List is empty.\n");
+		return;
+	}
+	temp = start;
+	start = start->link;
+	free(temp);
+	temp = NULL;
 	printf("First node deleted successfully.\n");
 }
 
-void sllDeleteAtEnd(){
-	struct node* temp, *prev;
-    if(start==NULL){
-        printf("List is empty.\n");
-        return;
-    }
-    temp=start;
-	while(temp->link!=NULL){
-        prev=temp;
-        temp=temp->link;
-    }
-	prev->link=NULL;
-    free(temp);
-    temp=NULL;
+void sllDeleteAtEnd()
+{
+	struct node *temp, *prev;
+	if (start == NULL)
+	{
+		printf("List is empty.\n");
+		return;
+	}
+	temp = start;
+	while (temp->link != NULL)
+	{
+		prev = temp;
+		temp = temp->link;
+	}
+	prev->link = NULL;
+	free(temp);
+	temp = NULL;
 	printf("Last node deleted successfully.\n");
 }
 
-void sllDeleteAtanyPos(int pos){
-	struct node* temp, *prev;
-    if(start==NULL){
-        printf("List is empty.\n");
-        return;
-    }
-    temp=start;
-    if(pos==1){
-        start=start->link;
-        free(temp);
-        temp=NULL;
-        printf("First node deleted successfully.\n");
-        return;
-    }
-    for(int i=1; i<=pos-2 && temp!=NULL; i++){
-        prev=temp;
-        temp=temp->link;
-    }
-    if(temp==NULL){
-        printf("Invalid Position!!!");
-        return;
-    }
-    prev->link=temp->link;
-    free(temp);
-    temp=NULL;
-    printf("Node deleted successfully at position %d.\n", pos);
+void sllDeleteAtanyPos(int pos)
+{
+	struct node *temp, *prev;
+	if (start == NULL)
+	{
+		printf("List is empty.\n");
+		return;
+	}
+	temp = start;
+	if (pos == 1)
+	{
+		start = start->link;
+		free(temp);
+		temp = NULL;
+		printf("First node deleted successfully.\n");
+		return;
+	}
+	for (int i = 1; i <= pos - 2 && temp != NULL; i++)
+	{
+		prev = temp;
+		temp = temp->link;
+	}
+	if (temp == NULL)
+	{
+		printf("Invalid Position!!!");
+		return;
+	}
+	prev->link = temp->link;
+	free(temp);
+	temp = NULL;
+	printf("Node deleted successfully at position %d.\n", pos);
 }
 
-void sllDisplay(){
-	struct node* ptr;
+void sllDisplay()
+{
+	struct node *ptr;
 
-	if(start==NULL){
+	if (start == NULL)
+	{
 		printf("Empty Linked List !!!");
 		return;
 	}
-	
-	ptr=start;
+
+	ptr = start;
 	printf("Start->");
-	while(ptr!=NULL){
-		printf(" %d ->",ptr->data);
-		ptr=ptr->link;
+	while (ptr != NULL)
+	{
+		printf(" %d ->", ptr->data);
+		ptr = ptr->link;
 	}
 	printf(" NULL");
 }
 
-int main(void){
+void FindMaxMin()
+{
+	struct node *ptr;
+	int max, min;
+	max = min = start->data;
+	ptr = start->link;
+	if (start == NULL)
+	{
+		printf("List is empty.\n");
+	}
+	else
+	{
+		while (ptr != NULL)
+		{
+			if (max < ptr->data)
+			{
+				max = ptr->data;
+			}
+			else if (min > ptr->data)
+			{
+				min = ptr->data;
+			}
+			ptr = ptr->link;
+		}
+	}
+	printf("Maximum value: %d\n", max);
+	printf("Minimum value: %d\n", min);
+}
+
+int CountNodes()
+{
+	struct node *ptr;
+	int count = 0;
+	if (start == NULL)
+	{
+		return 0;
+	}
+	ptr = start;
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->link;
+	}
+	return count;
+}
+
+int main(void)
+{
 	int ch, item, pos;
 	printf("------------Singly Linked List Operations: --------------\n");
-	while(1){
+	while (1)
+	{
 		printf("\n1. Insert at begin\n");
 		printf("2. Insert at end\n");
 		printf("3. Insert at specific position\n");
@@ -156,51 +230,58 @@ int main(void){
 		printf("5. Delete at end\n");
 		printf("6. Delete at specific position\n");
 		printf("7. Display\n");
-		printf("8. Exit\n");
+		printf("8. Find Max & Min Value\n");
+		printf("9. Count the Nodes\n");
+		printf("10. Exit\n");
 		printf("Enter your choice: ");
-		scanf("%d",&ch);
-		
-		switch(ch){
-			case 1:
-		                printf("Enter the item : ");
-		                scanf("%d",&item);
-		                sllInsertAtBegin(item);
-		                break;
-			case 2:
-		                printf("Enter the item : ");
-		                scanf("%d",&item);
-		                sllInsertAtEnd(item);
-		                break;
-			case 3:
-			    	printf("Enter the position : ");
-				scanf("%d",&pos);
-				printf("Enter the item : ");
-               			scanf("%d",&item);
-                		sllInsertAtanyPos(pos, item);
-				break;
-			case 4:
-			    	sllDeleteAtBegin();
-				break;
-			case 5:
-				sllDeleteAtEnd();
-				break;
-			case 6:
-				printf("Enter the position : ");
-				scanf("%d",&pos);
-				sllDeleteAtanyPos(pos);
-				break;
-			case 7:
-		                sllDisplay();
-		                break;
-			case 8:
-				printf("Thank you for using !! Bye.");
-				exit(0);
-            default:
-				printf("Wrong option !!! Please select a valid option\n");
+		scanf("%d", &ch);
+
+		switch (ch)
+		{
+		case 1:
+			printf("Enter the item : ");
+			scanf("%d", &item);
+			sllInsertAtBegin(item);
+			break;
+		case 2:
+			printf("Enter the item : ");
+			scanf("%d", &item);
+			sllInsertAtEnd(item);
+			break;
+		case 3:
+			printf("Enter the position : ");
+			scanf("%d", &pos);
+			printf("Enter the item : ");
+			scanf("%d", &item);
+			sllInsertAtanyPos(pos, item);
+			break;
+		case 4:
+			sllDeleteAtBegin();
+			break;
+		case 5:
+			sllDeleteAtEnd();
+			break;
+		case 6:
+			printf("Enter the position : ");
+			scanf("%d", &pos);
+			sllDeleteAtanyPos(pos);
+			break;
+		case 7:
+			sllDisplay();
+			break;
+		case 8:
+			FindMaxMin();
+			break;
+		case 9:
+			printf("The Number of Nodes: %d", CountNodes());
+			break;
+		case 10:
+			printf("Thank you for using !! Bye.");
+			exit(0);
+		default:
+			printf("Wrong option !!! Please select a valid option\n");
 		}
 	}
-		
+
 	return 0;
 }
-
-
