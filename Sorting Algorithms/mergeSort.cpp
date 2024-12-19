1faf4713eaaba195b0032c8b12dc4ce7856332db
Swaraj Das <swarajdas625w@gmail.com>
2024-12-19 11:55:41 +0530

    Added Merge Sort

---
+++ Sorting Algorithms/mergeSort.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int leftArr[n1], rightArr[n2];
+
+    for (int i = 0; i < n1; i++)
+        leftArr[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        rightArr[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k++] = leftArr[i++];
+        } else {
+            arr[k++] = rightArr[j++];
+        }
+    }
+
+    while (i < n1) {
+        arr[k++] = leftArr[i++];
+    }
+
+    while (j < n2) {
+        arr[k++] = rightArr[j++];
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);        
+        mergeSort(arr, mid + 1, right);  
+        merge(arr, left, mid, right);
+    }     
+}
+
+int main() {
+    int arr[10];
+    int n;
+    cout<<"Enter the array size <=10:";
+    cin >> n;
+    if (n > 10 || n <= 0) {
+        cout << "Invalid size. Please enter a size between 1 and 10." << endl;
+        return 1;
+    }
+    cout << "Enter the array elements: ";
+    for (int i = 0; i < n; i++) {
+        cout << "-> ";
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
